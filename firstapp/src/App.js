3b46914084748b7a50987bbd183e5d555f1ccf03
conrad <conjamalex@gmail.com>
2023-06-05 22:13:37 -0400

    Created and imported component

--- firstapp/src/App.js
+++ firstapp/src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
 
-function Header() {
-  return <h1>Hello World!</h1>;
-}
+import Heading from './components/Heading';
 
 function App() {
-  return <Header />;
+  return <Heading />;
 }
 
 export default App;
