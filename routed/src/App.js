f092012a3f15490b61db1660a19ec2b641dd5c94
conrad <conjamalex@gmail.com>
2023-06-17 15:20:06 -0400

    Added bird sounds

--- routed/src/App.js
+++ routed/src/App.js
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from "react-router-dom";
 import About from './components/About';
 import HomePage from './components/HomePage';
 import Contact from './components/Contact';
+import Sound from './components/Sound';
 
 function App() {
   return (
@@ -11,12 +12,14 @@ function App() {
         <Link to="/" className='nav-item'>Home</Link>
         <Link to="/about" className='nav-item'>About</Link>
         <Link to="/contact" className='nav-item'>Contact</Link>
+        <Link to="/sound" className='nav-item'>Sound</Link>
       </nav>
 
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/about" element={<About />}></Route>
         <Route path="/contact" element={<Contact />}></Route>
+        <Route path="/sound" element={<Sound />}></Route>
       </Routes>
 
       
